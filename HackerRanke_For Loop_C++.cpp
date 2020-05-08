27e4da43b5f421b6e5210cba4aec56f4175938ad
NayemHossain721 <nayem.hossain7298@gmail.com>
2020-05-09 03:01:10 +0600

    Create HackerRanke_For Loop_C++.cpp

---
+++ HackerRanke_For Loop_C++.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin>> a >> b;
+    string arr[9]= {"one","two","three","four","five","six","seven","eight","nine"};
+    for (int i=a; i<=b; i++)
+    {
+        if(i <= 9)
+        {
+            cout << arr[i-1] << endl;
+        }
+        else
+        {
+            if(i%2 ==0)
+            {
+                cout << "even" << endl;
+            }
+            else
+            {
+                cout << "odd" << endl;
+            }
+        }
+    }
+    return 0;
+}
